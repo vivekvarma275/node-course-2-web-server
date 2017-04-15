7e04f2495b1d6192ae87c9b6edaa945512723fe5
Vivek Varma <vivek.varma2006@gmail.com>
2017-04-15 23:23:29 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance');
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+// root/index ROUTE
+app.get('/', (req, res)=>{
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the Home page!'
+    });
+});
+
+// about ROUTE
+app.get('/about', (req, res)=>{
+    res.render('about', {
+        pageTitle: 'About Page'
+    });
+});
+
+// handle bad requests
+app.get('/bad', (req, res)=>{
+    res.send({
+        errorMessage: 'Unable to fulfill this request'
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000');
+});
